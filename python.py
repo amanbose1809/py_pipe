43cd3cac06de5a6b07a326ed1917cab3c21f4b3e
amanbose231 <amanbose231@gmail.com>
2026-06-12 12:37:00 +0530

    second change

--- python.py
+++ python.py
@@ -3,6 +3,6 @@ app=Flask(__name__)
 
 @app.route("/",methods=["GET"])
 def root():
-    return "flask/python app running on port:5500-> 7700"
+    return "flask/python app running on port: 9600"
 
 app.run(port=5500,host="0.0.0.0")
